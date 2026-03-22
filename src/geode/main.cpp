d03b98ed507daa5b8d1e59bc1f65957b7b46bf9d
vexi <vexithereal@gmail.com>
2026-03-22 11:17:34 +0000

    Update main.cpp

--- src/geode/main.cpp
+++ src/geode/main.cpp
@@ -3,11 +3,12 @@
 #include <Geode/loader/SettingEvent.hpp>
 
 $on_mod(Loaded) {
+    // This starts the OpenHack logic
     openhack::initialize();
 
-    // Allow user to change between OpenGL 2.0/3.0
     auto *mod = geode::Mod::get();
     ImGuiCocos::get().setForceLegacy(mod->getSettingValue<bool>("legacy-render"));
+    
     geode::listenForSettingChanges<bool>("legacy-render", [](bool value) {
         ImGuiCocos::get().setForceLegacy(value);
     });
